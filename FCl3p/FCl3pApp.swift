7c58a214fe1d3f1ecbc054123ffe61aa7cf43bc4
e_geneste <eleazargeneste@icloud.com>
2023-10-10 14:03:44 -0400

    Initial Commit

---
+++ FCl3p/FCl3pApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCl3pApp.swift
+//  FCl3p
+//
+//  Created by Eleazar Geneste on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FCl3pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
